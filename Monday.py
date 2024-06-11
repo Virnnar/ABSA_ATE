fd046ee0740625f235c23d5776dc502a8a713577
Virmar <virmarqy@gmail.com>
2024-06-12 04:59:45 +0800

    6.11

--- Monday.py
+++ Monday.py
@@ -4,8 +4,7 @@ if __name__ == '__main__':
     path = {"train": r"D:\\Code\\ABSA\\data\\train.raw", "test": "__"}
     data = f2v.file2data(path['train'])
 
-    vocab, idx_data, embedding = f2v.handle(data)
-
+    vocab, vocal_list, idx_data, embedding = f2v.handle(data)
     
 
 
